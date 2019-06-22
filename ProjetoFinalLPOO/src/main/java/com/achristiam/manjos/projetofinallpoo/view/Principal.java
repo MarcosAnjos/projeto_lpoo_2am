86111085bb193c5f3eb161a4ecae98cd07e0a254
Allainn Christiam <allainn@alunos.utfpr.edu.br>
2019-06-22 02:20:26 -0300

    Testes realizados

--- ProjetoFinalLPOO/src/main/java/com/achristiam/manjos/projetofinallpoo/view/Principal.java
+++ ProjetoFinalLPOO/src/main/java/com/achristiam/manjos/projetofinallpoo/view/Principal.java
@@ -9,12 +9,15 @@ import com.achristiam.manjos.projetofinallpoo.InicializarBanco;
 import com.achristiam.manjos.projetofinallpoo.controller.ClienteController;
 import com.achristiam.manjos.projetofinallpoo.controller.FuncionarioController;
 import com.achristiam.manjos.projetofinallpoo.controller.ProdutoController;
+import com.achristiam.manjos.projetofinallpoo.controller.UsuarioController;
 import com.achristiam.manjos.projetofinallpoo.controller.VendaController;
 import com.achristiam.manjos.projetofinallpoo.controller.VendaProdutoController;
 import com.achristiam.manjos.projetofinallpoo.model.dao.ConexaoHibernate;
+import com.achristiam.manjos.projetofinallpoo.model.dao.GenericDAO;
 import com.achristiam.manjos.projetofinallpoo.model.vo.Cliente;
 import com.achristiam.manjos.projetofinallpoo.model.vo.Funcionario;
 import com.achristiam.manjos.projetofinallpoo.model.vo.Produto;
+import com.achristiam.manjos.projetofinallpoo.model.vo.Usuario;
 import com.achristiam.manjos.projetofinallpoo.model.vo.Venda;
 import com.achristiam.manjos.projetofinallpoo.model.vo.VendaProduto;
 import java.util.List;
@@ -28,43 +31,69 @@ public class Principal {
     public static void main(String[] args) {
         InicializarBanco.inicializar();
         
-        
-        ClienteController cliC = new ClienteController();
+        //InicializarBanco.inicializarAdmin();
+        GenericDAO<Usuario> genericDao = new GenericDAO<Usuario>();
+        UsuarioController uc = new UsuarioController();
         FuncionarioController fc = new FuncionarioController();
-        ProdutoController pc = new ProdutoController();
-        VendaController vc = new VendaController();
-        VendaProdutoController vpc = new VendaProdutoController();
+        Usuario usr = new Usuario();
+        Funcionario fun1 = new Funcionario("João da Silva");
+        usr.setLogin("admin");
+        usr.setSenha("admin");
         
-        List<Cliente> clientes;
-        List<Funcionario> funcionarios;
-        List<Produto> produtos;
-        List<Venda> vendas;
-        List<VendaProduto> vendasProdutos;
+        usr.setFuncionario(fun1);
+        fun1.setUsuario(usr);
+
+        fc.gravar(fun1);        
+        uc.gravar(usr);
         
-        clientes = cliC.buscarTodos();
-        for(Cliente cliente:clientes){
-            System.out.println(cliente.toString());
-        }
         
-        funcionarios = fc.buscarTodos();
-        for(Funcionario funcionario:funcionarios){
-            System.out.println(funcionario.toString());
-        }
+        //usr = (Usuario) genericDao.listOne("usr_codigo", 1, Usuario.class);
         
-        produtos = pc.buscarTodos();
-        for(Produto produto:produtos){
-            System.out.println(produto.toString());
-        }
+        usr = uc.buscar("admin");
+        fun1 = fc.buscarUm(4L);
         
-        vendas = vc.buscarTodos();
-        for(Venda venda:vendas){
-            System.out.println(venda.toString());
-        }
+        //System.out.println(usr.getClass().getSimpleName());
+
+        System.out.println(usr.toString());
+        System.out.println(fun1.toString());
         
-        vendasProdutos = vpc.buscarTodos();
-        for(VendaProduto vendaProd:vendasProdutos){
-            System.out.println(vendaProd.toString());
-        }
+        
+//        ClienteController cliC = new ClienteController();
+//        FuncionarioController fc = new FuncionarioController();
+//        ProdutoController pc = new ProdutoController();
+//        VendaController vc = new VendaController();
+//        VendaProdutoController vpc = new VendaProdutoController();
+//        
+//        List<Cliente> clientes;
+//        List<Funcionario> funcionarios;
+//        List<Produto> produtos;
+//        List<Venda> vendas;
+//        List<VendaProduto> vendasProdutos;
+//        
+//        clientes = cliC.buscarTodos();
+//        for(Cliente cliente:clientes){
+//            System.out.println(cliente.toString());
+//        }
+//        
+//        funcionarios = fc.buscarTodos();
+//        for(Funcionario funcionario:funcionarios){
+//            System.out.println(funcionario.toString());
+//        }
+//        
+//        produtos = pc.buscarTodos();
+//        for(Produto produto:produtos){
+//            System.out.println(produto.toString());
+//        }
+//        
+//        vendas = vc.buscarTodos();
+//        for(Venda venda:vendas){
+//            System.out.println(venda.toString());
+//        }
+//        
+//        vendasProdutos = vpc.buscarTodos();
+//        for(VendaProduto vendaProd:vendasProdutos){
+//            System.out.println(vendaProd.toString());
+//        }
         
         ConexaoHibernate.close();
     }
