b0c513fdedb8710ab60d09a2b250b96738db65fa
Allainn Christiam <allainn@alunos.utfpr.edu.br>
2019-06-22 02:20:02 -0300

    Adicionado a tela de login

---
+++ ProjetoFinalLPOO/src/main/java/com/achristiam/manjos/projetofinallpoo/view/Login.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.achristiam.manjos.projetofinallpoo.view;
+
+import com.achristiam.manjos.projetofinallpoo.InicializarBanco;
+import com.achristiam.manjos.projetofinallpoo.controller.SistemaController;
+import com.achristiam.manjos.projetofinallpoo.model.bo.ValidarUsuario;
+import com.achristiam.manjos.projetofinallpoo.model.dao.ConexaoHibernate;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javax.swing.JButton;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author allainnc
+ */
+public class Login extends JFrame {
+    
+    private JButton jbOk      = new JButton("Ok");
+    private JButton jbCancela = new JButton("Cancelar");
+    private JLabel jlLogin = new JLabel("Login");
+    private JLabel jlSenha = new JLabel("Senha");
+    private JTextField jtfLogin = new JTextField();
+    private JPasswordField jtfSenha = new JPasswordField();
+    
+    public Login(){
+        super("Login");
+        
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+
+        
+        this.setSize(240, 160);
+        this.setLocation((d.width/2) - (240/2), (d.height/2) - (160/2));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        
+        this.getContentPane().setLayout(null);
+        
+        this.getContentPane().add(jlLogin);
+        this.getContentPane().add(jtfLogin);
+        this.getContentPane().add(jlSenha);
+        this.getContentPane().add(jtfSenha);
+        this.getContentPane().add(jbOk);
+        this.getContentPane().add(jbCancela);
+        
+        jlLogin.setBounds(10, 10, 50, 30);
+        jtfLogin.setBounds(70, 10, 160, 30);
+        jlSenha.setBounds(10, 50, 50, 30);
+        jtfSenha.setBounds(70, 50, 160, 30);
+        jbOk.setBounds(10, 90, 100, 30);
+        jbCancela.setBounds(130, 90, 100, 30);
+        
+        jbOk.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ev) {
+                if(ValidarUsuario.validar(jtfLogin.getText(), jtfSenha.getText())){
+                    dispose();
+                    new Sistema();
+                }
+            }
+        });
+        
+        jbCancela.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ev) {
+                ConexaoHibernate.close();
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+        
+        this.setVisible(true);
+        
+    }
+    
+    public static void main(String[] args) {
+        SistemaController.iniciarSistema();
+        InicializarBanco.inicializarAdmin();
+        new Login();
+    }
+}
+
--- ProjetoFinalLPOO/src/main/java/com/achristiam/manjos/projetofinallpoo/view/Sistema.java
+++ ProjetoFinalLPOO/src/main/java/com/achristiam/manjos/projetofinallpoo/view/Sistema.java
@@ -2,8 +2,12 @@ package com.achristiam.manjos.projetofinallpoo.view;
 
 import com.achristiam.manjos.projetofinallpoo.InicializarBanco;
 import com.achristiam.manjos.projetofinallpoo.controller.SistemaController;
+import com.achristiam.manjos.projetofinallpoo.model.bo.SingletonUsuario;
 import com.achristiam.manjos.projetofinallpoo.model.dao.ConexaoHibernate;
+import com.achristiam.manjos.projetofinallpoo.model.vo.Usuario;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -22,12 +26,21 @@ public class Sistema extends JFrame {
 
     
     public Sistema() {
-        super("Projeto Base");
+        super("Sistemãooo");
         telaInterna = new JDesktopPane();
+        
+        Usuario usuario = SingletonUsuario.getInstance();
+        System.out.println(usuario.toString());
 
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        
         this.setSize(670, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocation((d.width/2) - (670/2), (d.height/2) - (500/2));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.getRootPane().setJMenuBar(jmBarraMenus);
+
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         // exibe a tela
         this.add(telaInterna);
@@ -74,10 +87,10 @@ public class Sistema extends JFrame {
         this.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        SistemaController.iniciarSistema();
-        InicializarBanco.inicializarAdmin();
-        new Sistema();
-        //ConexaoHibernate.close();
-    }
+//    public static void main(String[] args) {
+//        SistemaController.iniciarSistema();
+//        //InicializarBanco.inicializarAdmin();
+//        new Sistema();
+//        //ConexaoHibernate.close();
+//    }
 }
